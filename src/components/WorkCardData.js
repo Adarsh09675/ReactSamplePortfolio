26cd65c0527e3040cded06fa4c7261881b142e97
Adarsh Mishra <akmsdr2019@gmail.com>
2025-10-06 01:28:56 +0530

    first commit

---
+++ src/components/WorkCardData.js
@@ -0,0 +1,28 @@
+//local api
+import pro1 from "../assets/project1.png"
+import pro2 from "../assets/project2.png"
+import pro3 from "../assets/project3.png"
+
+const ProjectCardData = [
+    {
+        imgsrc: pro1,
+        title: "Ecommerce Website Design",
+        text: "Thriving Facebook community and top-ranked Podcast,Any inspires a grounded,tangible and self-affirming sense of wow! really can do this for over 100,000 online entrepreneurs.Best-selling marketing courses",
+        view: "https://www.youtube.com/watch?v=P8YuWEkTeuE"
+
+    },
+    {
+        imgsrc: pro2,
+        title: "LMS/Course Website Design",
+        text: "Podcast Thriving Facebook community and top-ranked Podcast,Any inspires a grounded,tangible and self-affirming sense of wow! really can do this for over 100,000 online entrepreneurs.Best-selling marketing courses",
+        view: "https://www.youtube.com/watch?v=P8YuWEkTeuE"
+    },
+    {
+        imgsrc: pro3,
+        title: "Rymo Ecommerce Design",
+        text: "Top-Ranked Thriving Facebook community and top-ranked Podcast,Any inspires a grounded,tangible and self-affirming sense of wow! really can do this for over 100,000 online entrepreneurs.Best-selling marketing courses",
+        view: "https://www.youtube.com/watch?v=P8YuWEkTeuE"
+    },
+
+];
+export default ProjectCardData;
